fd4ae6c7bb713f42e31f14517f674cbffb719682
Shuliangfu <admin@shuliangfu.com>
2026-06-24 23:05:03 +0800

    refactor(compiler): update compiler/src/main_sx.c

--- compiler/src/main_sx.c
+++ compiler/src/main_sx.c
@@ -4,7 +4,7 @@
 extern int main_entry(int argc, char **argv);
 
 int main(int argc, char **argv) {
-    /* _exit to bypass atexit/global-dtor crash in SU self-hosted build */
+    /* _exit to bypass atexit/global-dtor crash in SX self-hosted build */
     int r = main_entry(argc, argv);
     _exit(r);
 }
